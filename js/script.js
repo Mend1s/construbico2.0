8083d374b1412c4b710d3de2c7b468f9dde351ce
Mend1s <andremendesskt99@gmail.com>
2023-07-28 22:34:21 -0300

    adicionando hero e demais ajustes do menu

--- js/script.js
+++ js/script.js
@@ -1,29 +1,17 @@
 const btnMobile = document.getElementById('btn-mobile');
-const nav = document.getElementById('nav');
-const menuItems = document.querySelectorAll('#menu a');
 
 function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault();
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
-    if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else {
-        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-    }
-}
-
-function closeMenu() {
-    nav.classList.remove('active');
-    btnMobile.setAttribute('aria-expanded', 'false');
-    btnMobile.setAttribute('aria-label', 'Abrir Menu');
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
-
-// Adicione ouvintes de evento para fechar o menu quando um item do menu for clicado
-menuItems.forEach(item => {
-    item.addEventListener('click', closeMenu);
-});
